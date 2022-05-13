f27ab127206ec01eb7f4d7814d5f6e1da26759d5
BB-choi <choibobae0809@gmail.com>
2022-05-13 23:42:12 +0900

    feat: express 서버 설치

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3100;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server running on port ${port}`)
+);
