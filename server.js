0ddbde29b5840545bd9d3f063d8067448f9cd32d
BB-choi <choibobae0809@gmail.com>
2022-05-14 15:57:32 +0900

    fix: express 라우터 설정 오류 해결 및 미들웨어 설정

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require("path");
 const app = express();
 const port = 3100;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });
 
 app.listen(process.env.PORT || port, () =>
